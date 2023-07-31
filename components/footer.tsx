1a1beb9667a8ace55b5ae1ab03c6636ace92e746
tinsaemesfin <qmesfin@gmail.com>
2023-07-31 12:55:26 +0300

    home page almost don product card done and designing  done

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white boarder-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+            &copy; 2023 Arada Mart , Inc All right reserved. 
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
